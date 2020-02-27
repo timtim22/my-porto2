08d7fd0a9baee08567ad213653315123047e9c8d
Lance Cable <lance@Lances-MacBook-Air.local>
2020-02-27 12:35:54 +0500

    added portfolio items via resource g

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_27_071235) do
+ActiveRecord::Schema.define(version: 2020_02_27_073308) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.text "body"
+    t.text "main_image"
+    t.text "thumb_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
